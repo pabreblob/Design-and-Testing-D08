474923a5aa457248fbe03d84cb46446fa34ba1ca
pabreblob <pabreblob@alum.us.es>
2018-02-24 10:58:52 +0100

    Proyecto y archivos varios
    Junto al código del proyecto, están los diagramas y el texto legal.

---
+++ Acme-Rendezvous/src/main/webapp/views/master-page/footer.jsp
@@ -0,0 +1,55 @@
+<%--
+ * footer.jsp
+ *
+ * Copyright (C) 2017 Universidad de Sevilla
+ * 
+ * The use of this project is hereby constrained to the conditions of the 
+ * TDG Licence, a copy of which you may download from 
+ * http://www.tdg-seville.info/License.html
+ --%>
+
+<%@page language="java" contentType="text/html; charset=ISO-8859-1"	pageEncoding="ISO-8859-1"%>
+
+<%@taglib prefix="fmt" uri="http://java.sun.com/jsp/jstl/fmt" %>
+
+<jsp:useBean id="date" class="java.util.Date" />
+
+<hr />
+
+<b>Copyright &copy; <fmt:formatDate value="${date}" pattern="yyyy" /> Acme, Inc.</b>
+<br />
+<div style="background-color: grey; font-family: Arial; font-size: 30px" id="pruebacookie"></div>
+
+<script>
+$(document).ready(function() {
+	if (getCookie("accepted") !== ("true")) {
+		var language = getCookie("language");
+		if (language == "" || language === "en") {
+			document.getElementById("pruebacookie").innerHTML = "We use cookies to improve your experience. If you continue surfing this website, we consider that you accept their use.<br/><button type='button' onclick='acceptCookies()'>Accept</button>";
+		} else {
+			document.getElementById("pruebacookie").innerHTML = "Usamos cookies para mejorar su experiencia. Si continúa navegando en este sitio, consideramos que acepta su uso.<br/><button type='button' onclick='acceptCookies()'>Aceptar</button>";
+		}
+	}
+});
+
+function acceptCookies() {
+	document.cookie = "accepted=true; path=/";
+	document.getElementById("pruebacookie").innerHTML = "";
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
+</script>
---
+++ Acme-Rendezvous/src/test/java/services/CommentTest.java
@@ -0,0 +1,133 @@
+
+package services;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.Assert;
+
+import utilities.AbstractTest;
+import domain.Comment;
+import domain.Rendezvous;
+import domain.Reply;
+import domain.User;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = {
+	"classpath:spring/datasource.xml", "classpath:spring/config/packages.xml"
+})
+@Transactional
+public class CommentTest extends AbstractTest {
+
+	@Autowired
+	private CommentService		commentService;
+	@Autowired
+	private RendezvousService	rendezvousService;
+	@Autowired
+	private UserService			userService;
+	@Autowired
+	private ReplyService		replyService;
+
+
+	@Test
+	public void testCreate() {
+		super.authenticate("User1");
+		final Rendezvous r = this.rendezvousService.findOne(41);
+		final Comment res = this.commentService.create(r);
+		Assert.notNull(res);
+		super.unauthenticate();
+	}
+
+	@Test
+	public void testFindOne() {
+		final Comment res = this.commentService.findOne(48);
+		Assert.notNull(res);
+	}
+
+	@Test
+	public void testFindAll() {
+		final Collection<Comment> res = this.commentService.findAll();
+		Assert.notNull(res);
+		Assert.notEmpty(res);
+	}
+
+	@Test
+	public void testSave() {
+		super.authenticate("User1");
+		final Rendezvous r = this.rendezvousService.findOne(41);
+		final Comment res = this.commentService.create(r);
+		res.setText("texto");
+		final Comment saved = this.commentService.save(res, r);
+		Assert.notNull(saved);
+		Assert.isTrue(r.getComments().contains(saved));
+		super.unauthenticate();
+	}
+
+	@Test
+	public void testDelete() {
+		super.authenticate("Admin");
+		final Rendezvous r = this.rendezvousService.findOne(41);
+		final List<Comment> comments = new ArrayList<Comment>(r.getComments());
+		final Comment c = comments.get(0);
+		this.commentService.delete(c);
+		Assert.isTrue(!r.getComments().contains(c));
+		super.unauthenticate();
+	}
+
+	@Test
+	public void testFindCommentsByUserId() {
+		super.authenticate("User1");
+		final User u = this.userService.findByPrincipal();
+		final Collection<Comment> res = this.commentService.findCommentsByUserId(u.getId());
+		for (final Comment c : res)
+			Assert.isTrue(c.getAuthor().equals(u));
+	}
+
+	@Test
+	public void testFindCommentByReplyId() {
+		final Reply r = this.replyService.findOne(50);
+		final Comment c = this.commentService.findCommentByReplyId(50);
+		Assert.isTrue(c.getReplies().contains(r));
+
+	}
+	//Negative Tests
+
+	@Test
+	public void testDeleteUser() {
+		boolean check = true;
+		try {
+			super.authenticate("User1");
+			final Rendezvous r = this.rendezvousService.findOne(41);
+			final List<Comment> comments = new ArrayList<Comment>(r.getComments());
+			final Comment c = comments.get(0);
+			this.commentService.delete(c);
+			check = false;
+		} catch (final Throwable oops) {
+			Assert.isTrue(true);
+		}
+		Assert.isTrue(check);
+	}
+
+	@Test
+	public void testDeleteUnauthenticated() {
+		boolean check = true;
+		try {
+			final Rendezvous r = this.rendezvousService.findOne(41);
+			final List<Comment> comments = new ArrayList<Comment>(r.getComments());
+			final Comment c = comments.get(0);
+			this.commentService.delete(c);
+			check = false;
+		} catch (final Throwable oops) {
+			Assert.isTrue(true);
+		}
+		Assert.isTrue(check);
+	}
+
+}
