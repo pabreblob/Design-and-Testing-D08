7f8392d92128a86c8b6e92e82b0e3fc9da0d16c2
Adrián Cantón Fernández <adriancanton@gmail.com>
2018-02-27 12:47:18 +0100

    Solucionado controlador de terminos y condiciones y añadido mensajes que faltaban de la plantilla

--- Sample-Project-1.14/src/main/java/controllers/AbstractController.java
+++ Sample-Project-1.14/src/main/java/controllers/AbstractController.java
@@ -14,6 +14,7 @@ import org.apache.commons.lang.exception.ExceptionUtils;
 import org.springframework.stereotype.Controller;
 import org.springframework.util.ClassUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -29,6 +30,31 @@ public class AbstractController {
 		result.addObject("name", ClassUtils.getShortName(oops.getClass()));
 		result.addObject("exception", oops.getMessage());
 		result.addObject("stackTrace", ExceptionUtils.getStackTrace(oops));
+
+		return result;
+	}
+
+	@RequestMapping(value = "/misc/terms")
+	public ModelAndView terms() {
+		ModelAndView result;
+
+		result = new ModelAndView("misc/terms");
+
+		return result;
+	}
+	@RequestMapping(value = "/misc/contact")
+	public ModelAndView contact() {
+		ModelAndView result;
+
+		result = new ModelAndView("misc/contact");
+
+		return result;
+	}
+	@RequestMapping(value = "/misc/cookies")
+	public ModelAndView cookies() {
+		ModelAndView result;
+
+		result = new ModelAndView("misc/cookies");
 
 		return result;
 	}
